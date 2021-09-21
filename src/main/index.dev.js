80a07e62bfc616b9d6602c24205d2efb6b12acaa
zkh110119 <34805273+zkh110119@users.noreply.github.com>
2021-09-21 22:15:55 +0800

    developer mode vue-devTools install

--- src/main/index.dev.js
+++ src/main/index.dev.js
@@ -8,16 +8,20 @@
 /* eslint-disable */
 
 // Install `electron-debug` with `devtron`
-require('electron-debug')({ showDevTools: true })
+require('electron-debug')({showDevTools: true})
+const {BrowserWindow} = require('electron'),
+  path = require('path')
 
 // Install `vue-devtools`
 require('electron').app.on('ready', () => {
   let installExtension = require('electron-devtools-installer')
   installExtension.default(installExtension.VUEJS_DEVTOOLS)
-    .then(() => {})
+    .then(() => {
+    })
     .catch(err => {
       console.log('Unable to install `vue-devtools`: \n', err)
     })
+  BrowserWindow.addDevToolsExtension(path.resolve(__dirname, '../../devTools/chrome'))
 })
 
 // Require `main` process to boot app
